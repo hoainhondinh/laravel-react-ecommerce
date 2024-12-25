24e414e569a1cb3f950bedccbccad24818e0c203
hoainhondinh <hoainhondinh@gmail.com>
2024-12-25 11:29:04 +0700

    install spatie/permissions package and define role and permissions

---
+++ app/Enums/PermissionsEnum.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Enums;
+
+enum PermissionsEnum: string
+{
+    case ApproveVendor = 'ApproveVendor';
+    case SellProducts = 'SellProducts';
+    case BuyProducts = 'BuyProducts';
+
+}
